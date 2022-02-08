3840669b485f72352a2b70405af39929b61a1803
Ayrat <huzin.ayratik@email.com>
2022-02-08 21:20:35 +0500

    git_test

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import "../App.css";
+import React from "react";
 import Navbar from "./Navbar/Navbar";
 import Header from "./Header/Header";
 import Payment from "./Payment/Payment";
---
+++ src/components/Test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Test() {
+  return <div>some start text</div>;
+}
+
+export default Test;
